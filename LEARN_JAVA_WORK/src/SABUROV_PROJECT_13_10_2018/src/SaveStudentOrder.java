6cf4b2110eeb77b0e9fb3c47d3aff6318dc291d5
Shamil Zaripov <sergeevivan24@gmail.com>
2018-10-13 22:08:22 +0500

    https://www.youtube.com/watch?v=cwwHxEO-ruY&index=4&list=PLyxk-1FCKqockmP-fXZmHQ7UlYP3qvZRa

---
+++ LEARN_JAVA_WORK/src/SABUROV_PROJECT_13_10_2018/src/SaveStudentOrder.java
@@ -0,0 +1,12 @@
+package SABUROV_PROJECT_13_10_2018.src;
+
+/**
+ * Created by Isanbet on 13.10.2018.
+ */
+
+public class SaveStudentOrder {
+    public static void main(String[] args) {
+        System.out.println("Save student order is running");
+    }
+}
+
