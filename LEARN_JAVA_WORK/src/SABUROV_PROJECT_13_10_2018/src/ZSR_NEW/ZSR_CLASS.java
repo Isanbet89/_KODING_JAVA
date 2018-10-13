7247f4eac0e289861dfd6906bc49c413cce5895d
Shamil Zaripov <sergeevivan24@gmail.com>
2018-10-13 20:33:28 +0500

    Возвращение двух папок

---
+++ LEARN_JAVA_WORK/src/SABUROV_PROJECT_13_10_2018/src/ZSR_NEW/ZSR_CLASS.java
@@ -0,0 +1,12 @@
+package ZSR_NEW;
+
+/**
+ * Created by Isanbet on 13.10.2018.
+ */
+
+public class ZSR_CLASS {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ LEARN_JAVA_WORK/src/SABUROV_PROJECT_13_10_2018/src/ZSR_NEW/dop_new_class_q.java
@@ -0,0 +1,26 @@
+package ZSR_NEW;
+
+/**
+ * Created by Isanbet on 13.10.2018.
+ */
+
+public class dop_new_class_q {
+    public void math_ficha(){
+
+    }
+
+    public void math_ficha2_check(){
+
+    }
+
+    public void math_ficha2_super_new(){
+
+    }
+
+    public void math_ficha2_summer(){
+
+    }
+
+
+}
+
