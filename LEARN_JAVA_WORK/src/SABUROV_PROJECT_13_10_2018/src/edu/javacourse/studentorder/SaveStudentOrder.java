19aba29f9d497d94bc8120d018a9f024e5017b77
Shamil Zaripov <sergeevivan24@gmail.com>
2018-10-16 23:54:47 +0500

    Урок 11 от Антона Сабурова. Геттеры и сеттеры

--- LEARN_JAVA_WORK/src/SABUROV_PROJECT_13_10_2018/src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ LEARN_JAVA_WORK/src/SABUROV_PROJECT_13_10_2018/src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -9,16 +9,10 @@ import SABUROV_PROJECT_13_10_2018.src.edu.javacourse.studentorder.domain.Student
 public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.hFirstName = "Алексей";
-        so.hLastName = "Петров";
-        so.wFirstName = "Галина";
-        so.wLastName = "Петрова";
-
-        StudentOrder so1 = new StudentOrder();
-        so1.hFirstName = "Алексей";
-        so1.hLastName = "Сидоров";
-        so1.wFirstName = "Галина";
-        so1.wLastName = "Сидорова";
+        so.sethFirstName("Алексей");
+        so.sethLastName("Петров");
+        so.setwFirstName("Галина");
+        so.setwLastName("Петрова");
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
@@ -26,7 +20,7 @@ public class SaveStudentOrder {
 
     static long saveStudentOrder(StudentOrder studentOrder){
         long answer = 199;
-        System.out.println("SaveStudentOrder 1 " + studentOrder.hLastName);
+        System.out.println("SaveStudentOrder 1 " + studentOrder.gethLastName());
 
         return answer;
     }
