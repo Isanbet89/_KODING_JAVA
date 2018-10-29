6707284c6b684e8a386d1d3e640b8316ca13ee62
Shamil Zaripov <sergeevivan24@gmail.com>
2018-10-30 01:01:01 +0500

    Урок 12 от Антона Сабурова, часть 2. StudentOrder корректировка. SaveStudentOrder убираем старые ссылки на StudentOrder

--- LEARN_JAVA_WORK/src/SABUROV_PROJECT_13_10_2018/src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ LEARN_JAVA_WORK/src/SABUROV_PROJECT_13_10_2018/src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -9,10 +9,7 @@ import SABUROV_PROJECT_13_10_2018.src.edu.javacourse.studentorder.domain.Student
 public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.sethFirstName("Алексей");
-        so.sethLastName("Петров");
-        so.setwFirstName("Галина");
-        so.setwLastName("Петрова");
+
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
@@ -20,7 +17,7 @@ public class SaveStudentOrder {
 
     static long saveStudentOrder(StudentOrder studentOrder){
         long answer = 199;
-        System.out.println("SaveStudentOrder 1 " + studentOrder.gethLastName());
+        System.out.println("SaveStudentOrder");
 
         return answer;
     }
