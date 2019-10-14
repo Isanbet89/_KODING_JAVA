5365490297a4807bd37617991b76f1d65ef10096
Shamil Zaripov <sergeevivan24@gmail.com>
2019-10-15 00:26:35 +0500

    с чистого листа

---
+++ LEARN_JAVA_WORK/src/Vasilyev/glava_1/VSEM_PRIVET.java
@@ -0,0 +1,13 @@
+package Vasilyev.glava_1;
+
+/**
+ * Created by Isanbet on 15.10.2019.
+ */
+
+import javax.swing.*;
+public class VSEM_PRIVET {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Всем большой привет");
+    }
+}
+
